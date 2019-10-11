9b23fff3ced1c4371c548e61f3b475383bd7db9f
Doug Kintop <dkintop@hotmail.com>
2019-10-11 16:11:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,5 +77,14 @@ attr_accessor :id, :name, :breed
       new_dog
   end
   
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs 
+    WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name)  
+    binding.pry
+  end 
+  
     
 end 
