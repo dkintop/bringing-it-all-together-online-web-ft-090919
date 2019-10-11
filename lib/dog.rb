4032bbfed36e7123622c111bf9208c9bcb2eeaf4
Doug Kintop <dkintop@hotmail.com>
2019-10-11 15:10:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,10 @@ class Dog
 attr_accessor :id, :name, :breed 
 
   def initialize(attributes)
-  
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
+  
+  
   
 end 
